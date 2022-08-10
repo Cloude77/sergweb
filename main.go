2f216206d3bed55ade6de688b8bbfeff071d85b9
Sergey Ibatullin <neftianik@inbox.ru>
2022-08-10 09:28:18 +0300

    Create our first web server!

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+// function call answer request user
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
 func main() {
-	fmt.Print("Hello world.")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
