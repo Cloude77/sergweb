aad3d0d369661bbd9ecae81593ad58ba53cdfbeb
Sergey Ibatullin <neftianik@inbox.ru>
2022-08-17 10:03:31 +0300

    Added a contact page

--- main.go
+++ main.go
@@ -9,7 +9,15 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprintf(w, "<h1>Welcome to my awesome site. My  friend!</h1>")
+	//fmt.Fprintf(w, r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1>Welcome to my awesome site. My  friend!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	}
+
+	// fmt.Fprintf(w, "<h1>Welcome to my awesome site. My  friend!</h1>")
+	// fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
 func main() {
